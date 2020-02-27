0dfd39ab9d00b3773f0747d3ebbc52fc9f807b5b
JSU <nonexistent@jsu.edu>
2020-02-26 19:51:31 -0600

    Complete Calculator

--- app/src/main/java/com/example/lex_calculator/MainActivity.java
+++ app/src/main/java/com/example/lex_calculator/MainActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView input, signBox;
 
-    String sign, Function;
+    String sign, specialFunction;
     String value1, value2;
     Double num1, num2, result;
     boolean hasDot, isSpecialFunction;
@@ -32,33 +37,93 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetText18n")
 
     public void logs(View view){
-
+        isSpecialFunction = true;
+        specialFunction = "log";
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("log");
     }
     public void In(View view){
+        isSpecialFunction = true;
+        specialFunction = "In";
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("In");
+
 
     }
     public void xn(View view){
+        isSpecialFunction = true;
+        specialFunction = "power";
+        value1 = input.getText().toString();
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("x^");
+
 
     }
     public void root(View view){
+        isSpecialFunction = true;
+        specialFunction = "root";
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("√");
 
     }
     public void exe(View view){
+        isSpecialFunction = true;
+        specialFunction = "factorial";
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("!");
+
 
     }
     public void sine(View view){
+        isSpecialFunction = true;
+        specialFunction = "sin";
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("sin");
 
     }
     public void cosine(View view){
+        isSpecialFunction = true;
+        specialFunction = "cos";
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("cos");
 
     }
     public void tan(View view){
+        isSpecialFunction = true;
+        specialFunction = "tan";
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("tan");
 
     }
 
     public void DELETE(View view){
+        if(input.getText().equals("")) {
+            input.setText(null);
+        }
+            else
+            {
+                int len = input.getText().length();
+                String s = input.getText().toString();
+                if (s.charAt(len-1)=='.') {
+                    hasDot=false;
+                    input.setText(input.getText().subSequence(0,input.getText().length()-1));
+                }
+                else
+                {
+                    input.setText(input.getText().subSequence(0,input.getText().length()-1));
+                }
+            }
+        }
 
-    }
+
     @SuppressLint("SetText18n")
     public void nine(View view){
         input.setText(input.getText()+"9");
@@ -71,7 +136,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void add(View view){
-
+        sign = "+";
+        value1 = input.getText().toString();
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("+");
     }
 
     @SuppressLint("SetText18n")
@@ -85,8 +154,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void subtract(View view){
-
+        sign = "-";
+        value1 = input.getText().toString();
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("-");
     }
+
     @SuppressLint("SetText18n")
     public void five(View view){
         input.setText(input.getText()+"5");
@@ -123,21 +197,152 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void period(View view){
+        if(!hasDot){
+            if (input.getText().equals("")){
+                input.setText("0.");
+            }
+            else {
+                input.setText(input.getText()+".");
+            }
+            hasDot=true;
+        }
 
     }
     public void divide(View view){
+        sign = "/";
+        value1 = input.getText().toString();
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("/");
 
     }
     public void six(View view){
 
     }
     public void multiply(View view){
+        sign = "*";
+        value1 = input.getText().toString();
+        input.setText(null);
+        hasDot = false;
+        signBox.setText("*");
 
     }
     public void equal(View view){
+        if((specialFunction == null && sign == null) || (input.getText().equals(""))){
+            signBox.setText("Error");
+        }
+        else if (specialFunction != null){
+            switch (specialFunction){
+                default:
+                    break;
+                case "sin":
+                    value1 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    input.setText(Math.sin(num1)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+                case "cos":
+                    value1 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    input.setText(Math.cos(num1)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+                case "tan":
+                    value1 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    input.setText(Math.tan(num1)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+                case "root":
+                    value1 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    input.setText(Math.sqrt(num1)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+                case "log":
+                    value1 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    input.setText(Math.log10(num1)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+                case "In":
+                    value1 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    input.setText(Math.log(num1)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+                case "power":
+                    value2 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    num2 = Double.parseDouble((value2));
+                    input.setText(Math.pow(num1,num2)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+                case "factorial":
+                    value1 = input.getText().toString();
+                    num1 = Double.parseDouble((value1));
+                    int i = Integer.parseInt(value1)-1;
 
+                    while (i>0) {
+                        num1 = num1 *i;
+                        i--;
+                    }
+                    input.setText(Math.pow(num1,num2)+"");
+                    specialFunction = null;
+                    isSpecialFunction = false;
+                    signBox.setText(null);
+                    break;
+
+            }
+            if (sign != null){
+                value2 = input.getText().toString();
+
+                num1 = Double.parseDouble((value1));
+                num2 = Double.parseDouble((value2));
+                input.setText(null);
+
+                switch (sign){
+                    default:
+                        break;
+                    case "+":
+                        result = num1+num2;
+                        sign = null;
+                        signBox.setText(null);
+                        break;
+                    case "-":
+                        result = num1-num2;
+                        sign = null;
+                        signBox.setText(null);
+                        break;
+                    case "/":
+                        result = num1/num2;
+                        sign = null;
+                        signBox.setText(null);
+                        break;
+                    case "*":
+                        result = num1*num2;
+                        sign = null;
+                        signBox.setText(null);
+                        break;
+                }
+            }
+            else{
+                signBox.setText("Error");
+            }
+        }
     }
-
-
-
 }
